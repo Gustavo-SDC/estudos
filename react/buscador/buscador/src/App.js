051996568951911084c10fa7057a42628cd24946
Gustavo Santos de Campos <92994147+Gustavo-SDC@users.noreply.github.com>
2023-04-20 09:36:47 -0300

    Novo projeto React

---
+++ react/buscador/buscador/src/App.js
@@ -0,0 +1,22 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="Title">Teste Projeto</h1>
+
+      <div className="containerInput">
+        <input
+          type="text"
+          placeholders="Digite seu CEP..."
+        />
+      </div>
+
+      <button className="ButtonSearch">
+    
+      </button>
+
+    </div>
+  );
+}
+
+export default App;
